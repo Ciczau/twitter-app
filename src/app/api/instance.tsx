374435d46c471d56b55d789d8985e8f93122ec6e
Ciczau <123020783+Ciczau@users.noreply.github.com>
2023-09-03 21:43:33 +0200

    Update instance.tsx

--- src/app/api/instance.tsx
+++ src/app/api/instance.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://ciczau-twitter-backend-e83fca20f698.herokuapp.com',
+    withCredentials: true,
 });
 
 export default instance;
